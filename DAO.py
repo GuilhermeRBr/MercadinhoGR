111a61e8d09496eb2e4d8444ab24c09215e27af4
Guilherme R. <guilhermerebrito@gmail.com>
2025-04-02 21:21:37 -0300

    Adicionando mais classes

--- DAO.py
+++ DAO.py
@@ -6,16 +6,26 @@ from Models import Cliente
 class ClienteDAO:
     @classmethod
     def salvar_cliente(cls, cliente: Cliente):
-        with open('data/clientes.json', 'w', encoding='Utf-8' ) as arq:
-            json.dump(cliente.nome, arq, indent=4, ensure_ascii=False)
-            
+        clientes = []
+        with open('data/clientes.json', 'r', encoding='utf-8') as arq:
+            clientes = json.load(arq)
+
+        clientes.append({
+            'nome': cliente.nome,
+            'cpf': cliente.cpf,
+            'telefone': cliente.telefone,
+            'email': cliente.email,
+            'endereco': cliente.endereco,
+            'data_nascimento': cliente.data_nascimento,
+            'id_cliente': cliente.id_cliente
+        })
+        with open('data/clientes.json', 'w', encoding='utf-8') as arq:
+            json.dump(clientes, arq, indent=4, ensure_ascii=False)
 
     @classmethod
     def listar_clientes(cls):
-        clientes = []
-        with open('clientes.json', 'r') as arq:
-            for linha in arq:
-                cliente = Cliente.from_json(linha.strip())
-                clientes.append(cliente)
-        return clientes
+        with open('data/clientes.json', 'r', encoding='utf-8') as arq:
+            clientes = json.load(arq)
+            for cliente in clientes:
+                return Cliente(cliente['nome'], cliente['cpf'], cliente['telefone'], cliente['email'], cliente['endereco'], cliente['data_nascimento'], cliente['id_cliente'])
     
